5b9643eec8016b86d79c6a1e9790c37f1cbb6d19
kavinkumar06 <kavinm09@gmail.com>
2017-08-07 17:07:10 +0530

    Create Puzzle.java

---
+++ Puzzle.java
@@ -0,0 +1,40 @@
+# Puzzle
+import java.util.Scanner;
+import java.io.*;
+public class Puzzle {
+	static int CountOneBit(int start,int end){
+		int primeCount=0;
+		for(int i=start;i<=end;i++){
+			Boolean ans=true;
+			int half=i/2;
+		 for(int j=2;j<=half;j++){
+			 if(i%j==0){
+				 ans=false;
+				 break;
+			 }
+		 }
+			if(ans){
+				boolean onesCount=false;
+				String binary=Integer.toBinaryString(i);
+				for(int k=0;k<binary.length()-1;k++){
+					if(binary.charAt(k)=='1'&&binary.charAt(k+1)=='1'){
+						 onesCount=true;
+						break;
+					}
+				}
+				if(onesCount){
+					primeCount++;
+				}
+			}
+		}
+		return primeCount;
+	}
+
+	public static void main(String[] args) {
+		Scanner input=new Scanner(System.in);
+		System.out.println(CountOneBit(input.nextInt(),input.nextInt()));
+		input.close();
+
+	}
+
+}
